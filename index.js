8fcbba65309d14a57ef62ebcdc0ff60d753753e7
Ramez Amr Ibrahim <50588877+Ramez08@users.noreply.github.com>
2023-05-27 22:23:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
-const PORT = 8000
-const axios = require('axios')
-const cheerio = require('cheerio')
-const express = require('express')
+const PORT = 8000; // The port number that the server will listen on.
+const axios = require('axios'); // The axios library is used to make HTTP requests.
+const cheerio = require('cheerio'); // The cheerio library is used to parse HTML.
+const express = require('express'); // The express library is used to create a web server.
 
-const app = express()
+const app = express(); // Create an express application.
 
-//Insert the link you want to scrape
+// Insert the link you want to scrape.
 const baseurl = 'https://www.lttstore.com/collections/all';
 
 axios(baseurl)
   .then(response => {
+    // Get the HTML response from the axios request.
     const html = response.data;
+
+    // Create a Cheerio object from the HTML response.
     const site = cheerio.load(html);
+
+    // Create an empty array to store the products.
     const products = [];
-    //Eneter the main attribute for your scrape
+
+    // Iterate over all of the `a` elements in the HTML.
     site('a').each(function() {
-      const Product = site(this).text().trim(); // Apply the trim() method to remove leading/trailing whitespace
+      // Get the text content of the `a` element.
+      const Product = site(this).text().trim();
+
+      // Get the href attribute of the `a` element.
       const baseurl = site(this).attr('href');
-      //Enter what the link starts with for your scrape
+
+      // If the href attribute starts with `/products/`, then...
       if (baseurl && baseurl.startsWith('/products/')) {
+        // Create a new object to store the product information.
         const Url = 'https://www.lttstore.com' + baseurl;
         products.push({
           Product,
@@ -27,8 +38,10 @@ axios(baseurl)
       }
     });
 
+    // Log the products to the console.
     console.log(products);
   })
   .catch(err => console.log(err));
 
-app.listen(PORT, () => console.log('server running on PORT ' + PORT.toString()))
+// Start the server on the specified port.
+app.listen(PORT, () => console.log('server running on PORT ' + PORT.toString()));
